94e37b96af92390689df1154a9e6b89129ca2d56
AshishBansode22 <139750931+AshishBansode22@users.noreply.github.com>
2024-05-29 20:05:05 +0530

    Update __init__.py

--- new_etl_project/__init__.py
+++ new_etl_project/__init__.py
@@ -4,6 +4,7 @@ from . import assets_sample
 from .io import io_file
 from .jobs.test_job import asset_job
 from .schedules.test_schedule import everyday_12noon
+from .Sensors.demo_sensor import demo_sensor
 
 all_assets = load_assets_from_modules([assets_sample])
 
@@ -11,6 +12,7 @@ defs = Definitions(
     assets=all_assets,
     jobs=[asset_job],
     schedules=[everyday_12noon],
+    sensors=[demo_sensor],
     resources={
         "io_file" : io_file.LocalFileSystemIOManager(),
     }
