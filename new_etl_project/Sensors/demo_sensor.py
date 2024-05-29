8248442f8d90ed23805fa3d15d72cd3c969c7196
AshishBansode22 <139750931+AshishBansode22@users.noreply.github.com>
2024-05-29 21:31:00 +0530

    Update demo_sensor.py

--- new_etl_project/Sensors/demo_sensor.py
+++ new_etl_project/Sensors/demo_sensor.py
@@ -8,7 +8,7 @@ from dagster import (
 from new_etl_project.jobs.test_job import asset_job
 
 # Define the sensor
-@asset_sensor(asset_key=AssetKey("get_locations"), job=asset_job)
+@asset_sensor(asset_key=AssetKey("get_regionCodes"), job=asset_job)
 def demo_sensor(context: SensorEvaluationContext, asset_event: EventLogEntry):
     assert asset_event.dagster_event and asset_event.dagster_event.asset_key
     yield RunRequest(
