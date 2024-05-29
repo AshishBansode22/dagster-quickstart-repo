c905ee613e80db62b9cdf686f8258296a9cf1cbc
AshishBansode22 <139750931+AshishBansode22@users.noreply.github.com>
2024-05-29 20:02:01 +0530

    Create demo_sensor.py

---
+++ new_etl_project/Sensors/demo_sensor.py
@@ -0,0 +1,21 @@
+from dagster import (
+    AssetKey,
+    EventLogEntry,
+    RunConfig,
+    SensorEvaluationContext,
+    asset_sensor,
+)
+from new_etl_project.jobs.test_job import asset_job
+
+# Define the sensor
+@asset_sensor(asset_key=AssetKey("get_locations"), job=asset_job)
+def demo_sensor(context: SensorEvaluationContext, asset_event: EventLogEntry):
+    assert asset_event.dagster_event and asset_event.dagster_event.asset_key
+    yield RunRequest(
+        run_key=context.cursor,
+        run_config=RunConfig(
+            ops={
+                "my_op": {}  # Add any required config for your ops here
+            }
+        ),
+    )
